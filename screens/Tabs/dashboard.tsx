2b56fec52206bb3417d0916ce3806a1612d0e0a1
Kaifansari98 <kaifansari54540@gmail.com>
2025-07-17 14:49:56 +0530

    test commit

--- screens/Tabs/dashboard.tsx
+++ screens/Tabs/dashboard.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import Navbar from '@/components/generic/Navbar'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 
 export default function DashboardTabScreen() {
   return (
