61c44074680432258a067a50951c843b3536ce49
Kaifansari98 <kaifansari54540@gmail.com>
2025-07-28 17:19:39 +0530

    PDF Download has been completed

---
+++ utils/getAssetUrls.ts
@@ -0,0 +1,6 @@
+const BASE_ASSET_URL = "http://192.168.1.5:7777/assets/scan-and-pack/";
+
+export function ScanAndPackUrl(fileName: string): string {
+  if (!fileName) return ""; // fallback for missing logo
+  return `${BASE_ASSET_URL}${encodeURIComponent(fileName)}`;
+}
